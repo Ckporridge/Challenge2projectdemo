64b2e9965277903b531ef01b5a0ebfadfe148df6
Zhou Chengkun <Ckporridge@icloud.com>
2026-08-08 11:46:42 +0800

    Initial Commit

---
+++ Challenge 2 Demo Collab real/Challenge_2_Demo_Collab_realApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2_Demo_Collab_realApp.swift
+//  Challenge 2 Demo Collab real
+//
+//  Created by Chengkun on 8/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2_Demo_Collab_realApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
